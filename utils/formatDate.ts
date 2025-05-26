7d880ee98119b83775c1703dd09aa6d0a85a5531
Votre Nom <votre.email@example.com>
2025-05-26 16:46:06 +0200

    correction

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -2,11 +2,13 @@ export function formatDrawDate(date: string | Date) {
   const dateObj = new Date(date);
   const jours = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'];
   
+  const jour = jours[dateObj.getDay()];
   const dateFormatted = dateObj.toLocaleDateString('fr-FR', {
     day: 'numeric',
     month: 'long',
     year: 'numeric',
   });
   
-  return `${jours[dateObj.getDay()]}\n${dateFormatted}`;
+  // On force le format voulu avec un saut de ligne après le jour
+  return `${jour}\n${dateFormatted}`;
 } 
